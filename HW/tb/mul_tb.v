ecac39378b5cfe38a27816cbee91a0d2ab6ac350
Cadmium-CD <512256738@qq.com>
2020-04-21 00:27:27 -0500

    Update mul_tb.v
    Compute ned instead of ed

--- HW/tb/mul_tb.v
+++ HW/tb/mul_tb.v
@@ -1,51 +1,70 @@
 //Testbench
-module mul_tb;
+module wallace_tb;
 logic [15:0]  a,b;
 logic [31:0]  sum;
 
 logic[31:0] actual_output;
 logic[31:0] expect_output;
-logic[31:0] e_cn;
-logic[31:0] r_cn;
-real        avg_var;
-
+real        e_cn;
+real        r_cn;
+real        ed;
+real        avg_var1;
+real        avg_var2;
+real        diff;
+real        max_error;
+real        cn_t;
   
 
  
-  wallace_acc uut(.a(a), .b(b),.sum(sum));
+  wallace uut(.a(a), .b(b),.sum(sum));
  
 initial begin
     a = 0;
     b = 0;
   	e_cn = 0;
-    r_cn = 5000000;
-    avg_var = 0;
+    r_cn = 1000000;
+    ed = 0;
+  	avg_var1 = 0;
+  	avg_var2 = 0;
+    diff = 0;
+    max_error = 0;
+    cn_t = 0;
 
     #1
  
   repeat(r_cn) 
     begin
+    cn_t = cn_t + 1;
     a = $random; 
-	  b = $random;
+	b = $random;
 
     #1
     expect_output = a *b;
     actual_output = sum;
+   
     
-    avg_var = avg_var + real'(expect_output-actual_output)/ real'(expect_output); 
     if(expect_output != actual_output)
       begin
-        $display("E %b\nA %b\n", expect_output,actual_output);
-          e_cn = e_cn + 1;
+        //$display("E %b\nA %b\n", expect_output,actual_output);
+        diff = ((real'(expect_output) - real'(actual_output))>0) ?(real'(expect_output) - real'(actual_output)):(real'(actual_output) - real'(expect_output));
+        //$display("Diff", diff);
+        max_error = (max_error > diff)?max_error:diff;
+        if(cn_t < r_cn /2)
+          avg_var1 = avg_var1 + diff;
+        else
+          avg_var2 = avg_var2 + diff;
+        e_cn = e_cn + 1;
+
       end  
     end
     
   #1
   //Calculate average ED
-  avg_var = avg_var/r_cn;
+  ed = avg_var1/max_error/r_cn +  avg_var2/max_error/r_cn;
   #1
-  $display("Error/Total: %d/%d", e_cn,r_cn);
-  $display("Average variation: %f", avg_var);
+  $display("Error/Total: %f/%f", e_cn,r_cn);
+  $display("Max error: %f", max_error);
+  $display("NED: %f", ed);
 end
  
 endmodule
