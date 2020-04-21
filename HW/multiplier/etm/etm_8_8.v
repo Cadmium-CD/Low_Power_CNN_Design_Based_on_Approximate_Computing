402cd030ad20d5045b9f9c3e97931344d2b7dbc1
Jestice08 <34907435+Jestice08@users.noreply.github.com>
2020-04-21 18:41:18 -0500

    updated etm_8_8

--- HW/multiplier/etm/etm_8_8.v
+++ HW/multiplier/etm/etm_8_8.v
@@ -1,4 +1,4 @@
-module etm_8_8_etm(a[15:0], b[15:0], p[31:0])
+module etm_8_8_etm(input [15:0] a, [15:0] b, output [31:0] p);
 
 wallace_8 w8 (a[15:8], b[15:8], p[31:16]);
 unmul_8   u8 (a[7:0], b[7:0], p[15:0]);
