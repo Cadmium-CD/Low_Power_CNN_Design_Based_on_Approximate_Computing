6eb9baf98ac52ef97c92ca0b72abcbbda30369ca
xinweiliu930 <30900675+xinweiliu930@users.noreply.github.com>
2020-05-06 13:55:14 -0700

    Add files via upload

--- SW/multiplier/appmulti.py
+++ SW/multiplier/appmulti.py
@@ -5,14 +5,14 @@ def mymulti():
 	mul_in_a = torch.randint(low=-32768,high=32767,size=(3,3),dtype=torch.int32)
 	mul_in_a = mul_in_a.numpy()
 	mul_in_b = torch.randint(low=-32768,high=32767,size=(3,3),dtype=torch.int32)
-	mul_in_a = mul_in_a.numpy()
+	mul_in_b = mul_in_b.numpy()
 	sign_a = np.where(mul_in_a<0,0x8000,0)
 	sign_b = np.where(mul_in_b<0,0x8000,0)
 	sign = np.bitwise_xor(sign_a,sign_b)
 	mul_in_a = np.where(mul_in_a<0,abs(mul_in_a),mul_in_a)
 	mul_in_b = np.where(mul_in_b<0,abs(mul_in_b),mul_in_b)
 	
-	mask_1 = np.full(shape=mul_in_a.shape,0x7ff0)
+	mask_1 = np.full(mul_in_a.shape,0x7ff0)
 	par_a = np.bitwise_and(mul_in_a,mask_1)
 	par_b = np.bitwise_and(mul_in_b,mask_1)
 	par_re = np.multiply(par_a,par_b)
@@ -21,7 +21,7 @@ def mymulti():
 	par_a = np.leftshift(mul_in_a,4)
 	par_b = np.leftshift(mul_in_b,4)
 	par_c = np.bitwise_or(par_a,par_b)
-	mask_2 = np.full(shape=mul_in_a.shape,0x80)
+	mask_2 = np.full(mul_in_a.shape,0x80)
 	par_c_7 = np.bitwise_and(par_c,mask_2)
 	mask_2 = np.rightshift(mask_2,1)
 	par_c_6 = np.bitwise_and(par_c,mask_2)
@@ -48,7 +48,7 @@ def mymulti():
 	par_re = np.bitwise_or(par_re,sign)
 	
 	return par_re
-	
+mymulti()	
 	
 	
 	
