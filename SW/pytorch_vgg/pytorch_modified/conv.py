540a71301844af4a6753c90ff5b20b6c6bd12f16
Di Cheng <512256738@qq.com>
2020-05-06 18:58:48 -0500

    replace mul with eta mul

--- SW/pytorch_vgg/pytorch_modified/conv.py
+++ SW/pytorch_vgg/pytorch_modified/conv.py
@@ -962,8 +962,138 @@ class MyConv2d(_ConvNd):
  
         return fx_data  
 
+    def mul_eta(self, mul_in_a,mul_in_b):
+        '''
+        print("aprox mul")
+        re = np.multiply(mul_in_a,mul_in_b)
+        sign_a = np.where(mul_in_a<0,-1,1)
+        sign_b = np.where(mul_in_b<1,-1,1)
+        sign = np.multiply(sign_a,sign_b)
+        mul_in_a = np.where(mul_in_a<0,abs(mul_in_a),mul_in_a)
+        mul_in_b = np.where(mul_in_b<0,abs(mul_in_b),mul_in_b)
+        
+        mask_1 = np.full(mul_in_a.shape,0x7ff0)
+        par_a = np.bitwise_and(mul_in_a,mask_1)
+        par_b = np.bitwise_and(mul_in_b,mask_1)
+        par_re = np.multiply(par_a,par_b)
+        
+        
+        par_a = np.left_shift(mul_in_a,4)
+        par_b = np.left_shift(mul_in_b,4)
+        par_c = np.bitwise_or(par_a,par_b)
+        mask_2 = np.full(mul_in_a.shape,0x80)
+        par_c_7 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_6 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_5 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_4 = np.bitwise_and(par_c,mask_2)
+        par_c_6 = np.bitwise_or(par_c_7>>1,par_c_6)
+        par_c_5 = np.bitwise_or(par_c_6>>1,par_c_5)
+        par_c_4 = np.bitwise_or(par_c_5>>1,par_c_4)
+        par_c_3 = np.where(par_c_4<1,0,8)
+        par_c_2 = np.right_shift(par_c_3,1)
+        par_c_1 = np.right_shift(par_c_3,2)
+        par_c_0 = np.right_shift(par_c_3,4)
+        
+        par_re = np.bitwise_or(par_re,par_c_7)
+        par_re = np.bitwise_or(par_re,par_c_6)
+        par_re = np.bitwise_or(par_re,par_c_5)
+        par_re = np.bitwise_or(par_re,par_c_5)
+        par_re = np.bitwise_or(par_re,par_c_3)
+        par_re = np.bitwise_or(par_re,par_c_2)
+        par_re = np.bitwise_or(par_re,par_c_1)
+        par_re = np.bitwise_or(par_re,par_c_0)
+        par_re = np.multiply(par_re,sign)
+        par_re = np.cast['int32'](par_re)
+
+        #delete matric
+        del par_a 
+        del par_b 
+        del par_c 
+        del mask_2 
+        del par_c_7
+        del par_c_6
+        del par_c_5
+        del par_c_4
+        del par_c_3
+        del par_c_2
+        del par_c_1
+        del par_c_0
+        del re        
+        del sign_a 
+        del sign_b 
+        del sign
+        del mul_in_a
+        del mul_in_b
+
+
+        return par_re
+        '''
+        sign_a = np.where(mul_in_a<0,0x8000,0)
+        sign_b = np.where(mul_in_b<0,0x8000,0)
+        sign = np.bitwise_xor(sign_a,sign_b)
+        mul_in_a = np.where(mul_in_a<0,abs(mul_in_a),mul_in_a)
+        mul_in_b = np.where(mul_in_b<0,abs(mul_in_b),mul_in_b)
+        
+        mask_1 = np.full(mul_in_a.shape,0x7ff0)
+        par_a = np.bitwise_and(mul_in_a,mask_1)
+        par_b = np.bitwise_and(mul_in_b,mask_1)
+        par_re = np.multiply(par_a,par_b)
+        
+        
+        par_a = np.left_shift(mul_in_a,4)
+        par_b = np.left_shift(mul_in_b,4)
+        par_c = np.bitwise_or(par_a,par_b)
+        mask_2 = np.full(mul_in_a.shape,0x80)
+        par_c_7 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_6 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_5 = np.bitwise_and(par_c,mask_2)
+        mask_2 = np.right_shift(mask_2,1)
+        par_c_4 = np.bitwise_and(par_c,mask_2)
+        par_c_6 = np.bitwise_or(par_c_7>>1,par_c_6)
+        par_c_5 = np.bitwise_or(par_c_6>>1,par_c_5)
+        par_c_4 = np.bitwise_or(par_c_5>>1,par_c_4)
+        par_c_3 = np.where(par_c_4<1,0,8)
+        par_c_2 = np.right_shift(par_c_3,1)
+        par_c_1 = np.right_shift(par_c_3,2)
+        par_c_0 = np.right_shift(par_c_3,4)
+        
+        par_re = np.bitwise_or(par_re,par_c_7)
+        par_re = np.bitwise_or(par_re,par_c_6)
+        par_re = np.bitwise_or(par_re,par_c_5)
+        par_re = np.bitwise_or(par_re,par_c_4)
+        par_re = np.bitwise_or(par_re,par_c_3)
+        par_re = np.bitwise_or(par_re,par_c_2)
+        par_re = np.bitwise_or(par_re,par_c_1)
+        par_re = np.bitwise_or(par_re,par_c_0)
+        par_re = np.where(sign==0,par_re,~par_re+1)
+
+        #delete matric
+        del par_a 
+        del par_b 
+        del par_c 
+        del mask_2 
+        del par_c_7
+        del par_c_6
+        del par_c_5
+        del par_c_4
+        del par_c_3
+        del par_c_2
+        del par_c_1
+        del par_c_0
+        del sign_a 
+        del sign_b 
+        del sign
+        del mul_in_a
+        del mul_in_b
+        return par_re
+
     def add_loa(self, mul_res):
-        #print(mul_res.shape)
+        print("aprox adding")
         mask_mat = np.ones(shape=mul_res.shape, dtype=np.int32) #create mask for bit 0
         temp1 = np.bitwise_and(mul_res, mask_mat) #mask bit 0
         add_res = np.sum(temp1, axis=0) #sum bit 0
@@ -1030,9 +1160,11 @@ class MyConv2d(_ConvNd):
         ifmap     = np.tile(ifmap,kernel_h)
         ifmap     = ifmap.transpose([1,0,2]).reshape(ifmap.shape[1],-1)
         #print("mul...")
-        product = np.multiply(kernelmap,ifmap) 
+        product = self.mul_eta(kernelmap,ifmap) 
+        print(product.shape)
         #print("sum...")
         product = self.add_loa(product)
+        print(product.shape)
         #print("reshape...")
         product = np.reshape(product,(batch_size,kernel_h,ifmap_w))
 
