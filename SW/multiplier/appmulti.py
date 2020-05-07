44037fb442b7855561cb60172578abd1dc616a9f
Yi Zheng <yizheng@login1.maverick2.tacc.utexas.edu>
2020-05-06 20:22:04 -0500

    modified appmulti.py

--- SW/multiplier/appmulti.py
+++ SW/multiplier/appmulti.py
@@ -6,17 +6,19 @@ def mymulti():
         mul_in_a = mul_in_a.numpy()
         mul_in_b = torch.randint(low=-32768,high=32767,size=(3,3),dtype=torch.int32)
         mul_in_b = mul_in_b.numpy()
+
+        ref = np.multiply(mul_in_a, mul_in_b)
+        print(ref)
+
         sign_a = np.where(mul_in_a<0,0x8000,0)
         sign_b = np.where(mul_in_b<0,0x8000,0)
         sign = np.bitwise_xor(sign_a,sign_b)
         mul_in_a = np.where(mul_in_a<0,abs(mul_in_a),mul_in_a)
         mul_in_b = np.where(mul_in_b<0,abs(mul_in_b),mul_in_b)
         
-        mask_1 = np.full(mul_in_a.shape,0x7ff0)
-        par_a = np.bitwise_and(mul_in_a,mask_1)
-        par_b = np.bitwise_and(mul_in_b,mask_1)
+        par_a = np.bitwise_and(mul_in_a,0x7ff0)
+        par_b = np.bitwise_and(mul_in_b,0x7ff0)
         par_re = np.multiply(par_a,par_b)
-        
         
         par_a = np.left_shift(mul_in_a,4)
         par_b = np.left_shift(mul_in_b,4)
@@ -32,21 +34,16 @@ def mymulti():
         par_c_6 = np.bitwise_or(par_c_7>>1,par_c_6)
         par_c_5 = np.bitwise_or(par_c_6>>1,par_c_5)
         par_c_4 = np.bitwise_or(par_c_5>>1,par_c_4)
-        par_c_3 = np.where(par_c_4<1,0,8)
-        par_c_2 = np.right_shift(par_c_3,1)
-        par_c_1 = np.right_shift(par_c_3,2)
-        par_c_0 = np.right_shift(par_c_3,4)
-        
+        par_c_4 = np.where(par_c_4<1,0,31)
+
         par_re = np.bitwise_or(par_re,par_c_7)
         par_re = np.bitwise_or(par_re,par_c_6)
         par_re = np.bitwise_or(par_re,par_c_5)
         par_re = np.bitwise_or(par_re,par_c_4)
-        par_re = np.bitwise_or(par_re,par_c_3)
-        par_re = np.bitwise_or(par_re,par_c_2)
-        par_re = np.bitwise_or(par_re,par_c_1)
-        par_re = np.bitwise_or(par_re,par_c_0)
         par_re = np.where(sign==0,par_re,~par_re+1)
-        
+
+        print(par_re)
+
         return par_re
 mymulti()        
         
